343a7f7e420e73904434e30f246a83140ae6ea66
서가희 <kahee430@gmail.com>
2024-09-20 18:03:15 +0900

    [BT-19] Feat : 장바구니 구현 중 next.js config 파일에 외부 이미지 경로 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   // sassOptions: {
   //   includePaths: [path.join(__dirname, 'styles')], // 2. sassOptions 옵션 추가
   // },
+
+  images: {
+    domains: ['image.aladin.co.kr'], // 외부 이미지를 가져올 도메인 추가
+  },
 };
 
 export default nextConfig;
