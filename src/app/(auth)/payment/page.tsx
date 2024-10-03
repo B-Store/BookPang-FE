5a8cf0ffbd84f9f027aef73c323e63e0607876db
서가희 <kahee430@gmail.com>
2024-10-03 11:48:25 +0900

    [BP-129] Feat: 장바구니 구현 결제 페이지로 이동 완료

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 
 const PaymentPage = () => {
+  const searchParams = useSearchParams();
+  const itemsParam = searchParams.get("item"); // 쿼리에서 items 가져오기
+  const checkItems = itemsParam
+    ? JSON.parse(decodeURIComponent(itemsParam))
+    : [];
+
+  console.log("checkItems", checkItems);
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      {checkItems.map(item => {
+        return (
+          <div
+            key={item.itemId}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Image src={item.cover} alt="도서 이미지" width={56} height={88} />
+            <div className="cartItem-contents">
+              <div>[{item.categoryName}]</div>
+              <div>{item.title}</div>
+              <div
+                style={{ display: "flex", flexDirection: "row", gap: "30px" }}
+              >
+                <div> {item.priceSales}</div>
+              </div>
+            </div>
 
-export default PaymentPage
+            <div>
+              결제금액
+              <div>원</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default PaymentPage;
