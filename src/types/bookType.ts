57a40ed2f66037ab0326629371c4d29324621b8b
서가희 <kahee430@gmail.com>
2024-10-10 20:52:36 +0900

    [BP-19] Fix: 결제 페이지 데이터 전달 수정 예정

---
+++ src/types/bookType.ts
@@ -0,0 +1,10 @@
+export type BookList = {
+  title: string;
+  link: string;
+  author: string;
+  itemId: number;
+  priceSales: number;
+  priceStandard: number;
+  cover: string;
+  categoryName: string;
+};
